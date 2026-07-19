d127476aef976605e3af8b5d7a9338dca0bb6233
MoMo <8466103+songguo405@user.noreply.gitee.com>
2026-07-19 09:59:51 +0800

    day-1

---
+++ app/pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="title">about</div>
+    <NuxtLink to="/detail"> detail </NuxtLink><!-- This is a NuxtLink component that allows navigation to the homepage -->
+</template>
+<script lang="ts" setup>
+useSeoMeta({
+    title: 'About Page',
+    description: 'This is the about page of our Nuxt 3 application.',
+})
+</script>
+
+<style scoped>
+.title {
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <NuxtLink to="/about"> Go to About Page </NuxtLink><!-- This is a NuxtLink component that allows navigation to the about page -->
+  </div>
+</template>
